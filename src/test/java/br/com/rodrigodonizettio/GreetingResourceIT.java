56b1cc2f367be3567a32eb08d30f0061cc7bfafe
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-05-25 08:06:31 -0300

    Disabling integration test due it isn't used

--- src/test/java/br/com/rodrigodonizettio/GreetingResourceIT.java
+++ src/test/java/br/com/rodrigodonizettio/GreetingResourceIT.java
@@ -1,7 +1,9 @@
 package br.com.rodrigodonizettio;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled
 @QuarkusIntegrationTest
 public class GreetingResourceIT extends GreetingResourceTest {
 
