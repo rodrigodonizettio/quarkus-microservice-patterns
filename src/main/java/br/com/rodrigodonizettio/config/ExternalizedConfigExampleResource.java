795b18331838ccf9569bf9e9b4ed463d1f0e2b6c
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-05-25 08:24:49 -0300

    Adding Distributed Tracing for ExternalizedConfigExampleResource

--- src/main/java/br/com/rodrigodonizettio/config/ExternalizedConfigExampleResource.java
+++ src/main/java/br/com/rodrigodonizettio/config/ExternalizedConfigExampleResource.java
@@ -1,12 +1,14 @@
 package br.com.rodrigodonizettio.config;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.opentracing.Traced;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+@Traced
 @Path("/config")
 public class ExternalizedConfigExampleResource {
 
